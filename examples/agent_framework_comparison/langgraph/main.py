edb4bd3310adde02c2af2ba858287ff564c6ac42
Kaloyan <kaloyan@example.com>
2026-04-01 18:33:06 -0700

    Add docs and update dependencies for  local tests

--- examples/agent_framework_comparison/langgraph/main.py
+++ examples/agent_framework_comparison/langgraph/main.py
@@ -1,10 +1,18 @@
 import os
 import sys
-
-sys.path.insert(1, os.path.join(sys.path[0], ".."))
+import asyncio
+import logging
 import gradio as gr
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+from dotenv import load_dotenv
 from langgraph.router import run_agent
 from utils.instrument import Framework, instrument
+
+load_dotenv()
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
 
 
 def gradio_interface(message, history):
@@ -12,6 +20,17 @@ def gradio_interface(message, history):
 
 
 def launch_app():
+    """
+    Launches the LangGraph Copilot Agent application.
+
+    This function creates a new asyncio event loop, sets it as the current event loop for the thread,
+    and then launches the Gradio ChatInterface with the specified function and title.
+    """
+     # Create new event loop
+    loop = asyncio.new_event_loop()
+    # Set it as the current event loop for this thread
+    asyncio.set_event_loop(loop)
+
     iface = gr.ChatInterface(fn=gradio_interface, title="LangGraph Copilot Agent")
     iface.launch()
 
